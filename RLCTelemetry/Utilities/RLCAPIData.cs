4dd7022def23210a99934a49d751e359a8b98e20
Darryl Hamilton <darryl@addict.net.nz>
2014-04-28 18:52:35 +1200

    Add beginnings of API code

---
+++ RLCTelemetry/Utilities/RLCAPIData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RLCTelemetry.Utilities
+{
+    public class RLCAPIData
+    {
+        public int session_id { get; set; }
+        public List<string> drivers { get; set; }
+    }
+}
